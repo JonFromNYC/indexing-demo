1bbeb9e2343e4a9a51a90a38810087ab8dd99978
Jonathan G <82.garcia@gmail.com>
2025-05-01 15:22:22 -0500

    mod:indexer

--- app/indexer.py
+++ app/indexer.py
@@ -1,6 +1,6 @@
 import logging
 from typing import Union
-from app.client import get_api_client
+from app.validation import is_valid_document
 import glean_indexing_api_client as indexing_api
 
 def deliver(document: dict) -> dict | None:
@@ -10,9 +10,14 @@ def deliver(document: dict) -> dict | None:
     Args:
         document (dict): A dictionary representing a document.
 
-    Returns:
+    Returns
         dict | None: API response if successful, None if an error occurred.
     """
+    # Check if the document is valid. Stop processing if not.
+    if not is_valid_document(document):
+        logging.error("Invalid document, skipping indexing.")
+        return None
+    
     connection = indexing_api.DocumentsAPI(get_api_client())
 
     try:
@@ -30,7 +35,7 @@ def index_documents(documents: Union[dict, list[dict]]) -> None:
     Args:
         documents (dict or list[dict]): A single document or list of documents.
 
-    Returns:
+    Returns
         None
     """
     if isinstance(documents, dict):
